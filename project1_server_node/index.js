ecc5bc24df1b911270244d5f7e38c1205d7bca6a
andresh01 <andres85684@outlook.com>
2023-04-03 19:53:25 -0500

    adding patch method

--- project1_server_node/index.js
+++ project1_server_node/index.js
@@ -13,12 +13,12 @@ app.use(express.json());
 
 
 app.get("/api/v1/products", (req, res) => {
-    readProducts(req,res);
+    readProducts(req, res);
 })
 
 
 app.get("/api/v1/products/:productId", (req, res) => {
-    readProductsById(req,res);
+    readProductsById(req, res);
 })
 
 
@@ -31,8 +31,8 @@ app.delete("/api/v1/products/:productId", (req, res) => {
     deleteProduct(req, res);
 })
 
-app.patch("/api/v1/products/:productId", (req, res) => {
-    updateProduct(req,res);
+app.patch("/api/v1/products/", (req, res) => {
+    updateProduct(req, res);
 })
 
 // Read file .txt where is the products and it change from string to object /return a promise object
@@ -44,99 +44,89 @@ const readFile = async () => {
 
 
 //Check if product exists or not / return -1 if the id doesnot exists
-function productExist (objProducts, idInt){     
+function productExist(objProducts, idInt) {
     let productIndex = objProducts.products.map((element) => element.id).indexOf(idInt);
     return productIndex;
 }
 
-function readProducts (req, res) {
+function readProducts(req, res) {
     readFile().then((objProducts) => {
         res.send(objProducts.products)
     })
 }
 
-function readProductsById (req, res) {
+function readProductsById(req, res) {
     readFile().then((objProducts) => {
         const { productId } = req.params;
         const idInt = parseInt(productId);
         let productIndex = objProducts.products.map((element) => element.id).indexOf(idInt);
-        
+
         res.send(objProducts.products[productIndex]);
     })
 }
 
-function addProduct (req, res){
+function addProduct(req, res) {
     readFile().then((objProducts) => {
-        
+
         const product = req.body;
         const idInt = parseInt(product.id);
-        
-        if (productExist(objProducts,idInt) == -1) {
-            objProducts.products.push(product);
-        res.json(objProducts);
-        const productsString = JSON.stringify(objProducts);
-        
-        const writeFile = async () => {
-            const filePath = path.resolve(`${__dirname}/products.txt`);
-            const data = await fs.promises.writeFile(filePath, productsString);
-        }
-        writeFile();
-        } else {
-            res.json("Product already exist");
-        }
-        
-    })
-}
 
-function deleteProduct (req, res) {
-    readFile().then((objProducts) => {
-       
-        const { productId } = req.params;
-        const idInt = parseInt(productId);
-        
-        if (productExist(objProducts,idInt) != -1) {
-            objProducts.products.splice(productIndex, 1);
+        if (productExist(objProducts, idInt) == -1) {
+            objProducts.products.push(product);
             res.json(objProducts);
-            const productsString = JSON.stringify(objProducts)
-            
+            const productsString = JSON.stringify(objProducts);
+
             const writeFile = async () => {
                 const filePath = path.resolve(`${__dirname}/products.txt`);
                 const data = await fs.promises.writeFile(filePath, productsString);
-            } 
+            }
             writeFile();
-            
+        } else {
+            res.json("Product already exist");
+        }
+
+    })
+}
+
+function deleteProduct(req, res) {
+    readFile().then((objProducts) => {
+
+        const { productId } = req.params;
+        const idInt = parseInt(productId);
+
+        if (productExist(objProducts, idInt) != -1) {
+            objProducts.products.splice(productIndex, 1);
+            res.json(objProducts);
+            const productsString = JSON.stringify(objProducts)
+
+            const writeFile = async () => {
+                const filePath = path.resolve(`${__dirname}/products.txt`);
+                const data = await fs.promises.writeFile(filePath, productsString);
+            }
+            writeFile();
+
         } else {
             res.json("Product does not exist")
             console.log('Product does not exist')
         }
-        
+
     })
 }
 
-function updateProduct (req, res) {
-    const { productId } = req.params;
-    const idInt = parseInt(productId);
-    const productIndex = objProducts.products.map((element) => element.id).indexOf(idInt);
-    const product = objProducts.products[productIndex];
-    console.log("product: ", product);
+function updateProduct(req, res) {
 
-    const productModification = req.body;
-    console.log("producto modificado ", productModification);
-    const arrayKeys = Object.keys(productModification);
-    const arrayValues = Object.values(productModification);
-    const { name } = arrayKeys;
-    console.log();
-
-    console.log(product.name);
-
-
-    /* for (let i = 0; i < arrayKeys.length; i++) {
-        product.arrayKeys[i] = arrayValues[i];
+    readFile().then((objProducts) => {
         
-    }
-        */
+        const updateProduct = req.body;
+        const idInt = parseInt(updateProduct.id);
 
-    res.json(objProducts.products[productIndex])
+        if (productExist(objProducts, idInt) != -1) {
+            objProducts.products[productExist(objProducts, idInt)] = updateProduct;
+            res.json(objProducts);
+        } else {
+            res.json("Product does not exists")
+        }
+    })
 }
 
 app.listen(PORT, () => {
